1dcbc925069ef36071aac5f6290cc204218a845e
Julian Mierzejewski <j3mierz@gmail.com>
2024-04-16 23:46:57 +0200

    exercise 2 abstrakcja

--- 01_Dzien_01/02_Abstrakcja/02_Zadanie_2/exercise.py
+++ 01_Dzien_01/02_Abstrakcja/02_Zadanie_2/exercise.py
@@ -8,4 +8,28 @@ def middle_element(sequences):
     return result
 
 
-print(middle_element([[1, 4, 2, 6, 7], [3, 4, '3', 'siema', 32, 5], [5, 6]]))
+
+
+class SequencesOfNumbers:
+    def __init__(self, start, stop, step):
+        self.start = start
+        self.stop = stop
+        self.step = step
+
+    def __len__(self):
+        result = range(self.start, self.stop, self.step)
+        return len(result)
+
+    def __getitem__(self, index):
+        try:
+            result = range(self.start, self.stop, self.step)[index]
+        except IndexError:
+            raise IndexError
+        return result
+
+
+a = SequencesOfNumbers(14, 46, 4)
+print(len(a))
+print(a[7])
+
+print(middle_element([[1, 4, 2, 6, 7], [3, 4, '3', 'siema', 32, 5], [5, 6], a]))
